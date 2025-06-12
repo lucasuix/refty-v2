c9be49b666d60b64a0adad295949065334f8ab92
lucas <lucasvillani700@gmail.com>
2025-06-12 20:28:13 -0300

    Atualizado o arquivo BAT para leitura de Token

--- RFTOnline.bat
+++ RFTOnline.bat
@@ -1,6 +1,6 @@
 @echo off
 :: Variáveis
-set TOKEN=meu_token_read_only
+set TOKEN=decoy_token
 set REPO=https://%TOKEN%@github.com/lucasuix/refty-v2.git
 
 :: Atualiza do repositório remoto usando o token
